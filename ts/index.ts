4076e952d0186bb51a85af0f2c00e6e88b34ed02
TheConnMan <bcconn2112@gmail.com>
2017-12-09 11:56:04 -0500

    Simplity logger methods

--- ts/index.ts
+++ ts/index.ts
@@ -10,36 +10,27 @@ class SailsPersistenceLogger {
     this.options = new Options(overrides);
   }
 
-  public afterCreate(record, clazz): Promise<null> {
-    var me = this;
-    return new Promise((resolve, reject) => {
-      if (me.options.isActive(clazz.identity, 'CREATE')) {
-        me.options.logger[me.options.level]('Created ' + clazz.identity + ' ' + record[clazz.primaryKey]);
-      }
-      resolve();
-    });
+  public async afterCreate(record, clazz): Promise<void> {
+    if (this.options.isActive(clazz.identity, 'CREATE')) {
+      this.options.logger[this.options.level]('Created ' + clazz.identity + ' ' + record[clazz.primaryKey]);
+    }
+    return Promise.resolve();
   }
 
-  public afterUpdate(record, clazz): Promise<null> {
-    var me = this;
-    return new Promise((resolve, reject) => {
-      if (me.options.isActive(clazz.identity, 'UPDATE')) {
-        me.options.logger[me.options.level]('Updated ' + clazz.identity + ' ' + record[clazz.primaryKey]);
-      }
-      resolve();
-    });
+  public afterUpdate(record, clazz): Promise<void> {
+    if (this.options.isActive(clazz.identity, 'UPDATE')) {
+      this.options.logger[this.options.level]('Updated ' + clazz.identity + ' ' + record[clazz.primaryKey]);
+    }
+    return Promise.resolve();
   }
 
-  public afterDestroy(records, clazz): Promise<null> {
-    var me = this;
-    return new Promise((resolve, reject) => {
-      if (me.options.isActive(clazz.identity, 'DESTROY')) {
-        records.forEach(record => {
-          me.options.logger[me.options.level]('Destroyed ' + clazz.identity + ' ' + record[clazz.primaryKey]);
-        });
-      }
-      resolve();
-    });
+  public async afterDestroy(records, clazz): Promise<void> {
+    if (this.options.isActive(clazz.identity, 'DESTROY')) {
+      records.forEach((record) => {
+        this.options.logger[this.options.level]('Destroyed ' + clazz.identity + ' ' + record[clazz.primaryKey]);
+      });
+    }
+    return Promise.resolve();
   }
 }
 
