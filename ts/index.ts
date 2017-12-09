43ab79797295e3219e7606540dce3ea51eed54fe
TheConnMan <bcconn2112@gmail.com>
2017-12-09 12:02:35 -0500

    Refactor logger to consolidate duplicated code

--- ts/index.ts
+++ ts/index.ts
@@ -10,24 +10,22 @@ class SailsPersistenceLogger {
     this.options = new Options(overrides);
   }
 
-  public async afterCreate(record, clazz): Promise<void> {
-    if (this.options.isActive(clazz.identity, 'CREATE')) {
-      this.options.logger[this.options.level]('Created ' + clazz.identity + ' ' + record[clazz.primaryKey]);
-    }
-    return Promise.resolve();
+  public async afterCreate(record: object, clazz): Promise<void> {
+    return this.after([record], clazz, 'CREATE', 'Created');
   }
 
-  public afterUpdate(record, clazz): Promise<void> {
-    if (this.options.isActive(clazz.identity, 'UPDATE')) {
-      this.options.logger[this.options.level]('Updated ' + clazz.identity + ' ' + record[clazz.primaryKey]);
-    }
-    return Promise.resolve();
+  public afterUpdate(record: object, clazz): Promise<void> {
+    return this.after([record], clazz, 'UPDATE', 'Updated');
   }
 
-  public async afterDestroy(records, clazz): Promise<void> {
-    if (this.options.isActive(clazz.identity, 'DESTROY')) {
+  public async afterDestroy(records: object[], clazz): Promise<void> {
+    return this.after(records, clazz, 'DESTROY', 'Destroyed');
+  }
+
+  private async after(records: object[], clazz, action: string, message: string): Promise<void> {
+    if (this.options.isActive(clazz.identity, action)) {
       records.forEach((record) => {
-        this.options.logger[this.options.level]('Destroyed ' + clazz.identity + ' ' + record[clazz.primaryKey]);
+        this.options.logger[this.options.level](`${message} ${clazz.identity} ${record[clazz.primaryKey]}`);
       });
     }
     return Promise.resolve();
